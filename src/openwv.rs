hichchiputha/openwv

commit 3ba7dc0195188d11ff295ba2da13cc25d84abb81

Store keys in OpenWv, simplify code in Session

src/openwv.rs
@@ -8,6 +8,7 @@ use std::slice;
 use std::sync::OnceLock;
 
 use crate::ffi::cdm;
+use crate::keys::ContentKey;
 use crate::session::{Session, SessionStore};
 use crate::util::cstr_from_str;
 use crate::wvd_file;
@@ -120,6 +121,7 @@ extern "C" fn CreateCdmInstance(
         host,
         sessions: SessionStore::new(),
         device,
+        keys: vec![],
         allow_persistent_state: false,
         cpp_peer: Default::default(),
     });
@@ -146,6 +148,7 @@ use crate::ffi;
 pub struct OpenWv {
     host: Pin<&'static mut cdm::Host_10>,
     sessions: SessionStore,
+    keys: Vec<ContentKey>,
     device: &'static wvd_file::WidevineDevice,
     allow_persistent_state: bool,
 }
@@ -305,17 +308,17 @@ impl cdm::ContentDecryptionModule_10_methods for OpenWv {
         };
 
         let response_raw = unsafe { slice::from_raw_parts(response, response_size as _) };
-        if let Err(e) = sess.update(response_raw) {
+        if let Err(e) = sess.load_license_keys(response_raw, &mut self.keys) {
             self.throw(promise_id, &e);
             return;
         }
 
         self.host.as_mut().OnResolvePromise(promise_id);
 
-        let keys = sess.content_keys();
-        if !keys.is_empty() {
+        if !self.keys.is_empty() {
             // Build an array of KeyInformation structs that point into keys.
-            let key_infos: Vec<cdm::KeyInformation> = keys
+            let key_infos: Vec<cdm::KeyInformation> = self
+                .keys
                 .iter()
                 .map(|k| cdm::KeyInformation {
                     key_id: k.data.as_ptr(),

src/session.rs
@@ -113,7 +113,6 @@ pub struct Session {
     id: SessionId,
     device: &'static WidevineDevice,
     keys: KeyState,
-    content_keys: Vec<ContentKey>,
 }
 
 impl Session {
@@ -122,7 +121,6 @@ impl Session {
             id: SessionId::generate(),
             device,
             keys: KeyState::NotSet,
-            content_keys: vec![],
         }
     }
 
@@ -168,7 +166,11 @@ impl Session {
         })
     }
 
-    pub fn update(&mut self, response_raw: &[u8]) -> Result<(), LicenseError> {
+    pub fn load_license_keys(
+        &mut self,
+        response_raw: &[u8],
+        keys: &mut Vec<ContentKey>,
+    ) -> Result<bool, LicenseError> {
         let response = video_widevine::SignedMessage::decode(response_raw)?;
 
         if response.r#type != Some(video_widevine::signed_message::MessageType::License as i32) {
@@ -180,14 +182,15 @@ impl Session {
         };
 
         let padding = Oaep::new::<sha1::Sha1>();
-        let key = self.device.private_key.decrypt(padding, &wrapped_key)?;
-        let keys = self.keys.finish_initialization(&key)?;
+        let session_key = self.device.private_key.decrypt(padding, &wrapped_key)?;
+        let session_keys = self.keys.finish_initialization(&session_key)?;
 
         let Some(license_raw) = response.msg else {
             return Err(LicenseError::NoLicense);
         };
 
-        let mut digester = hmac::Hmac::<sha2::Sha256>::new_from_slice(&keys.mac_server).unwrap();
+        let mut digester =
+            hmac::Hmac::<sha2::Sha256>::new_from_slice(&session_keys.mac_server).unwrap();
         digester.update(&license_raw);
         let expected_sig = digester.finalize().into_bytes();
 
@@ -201,19 +204,15 @@ impl Session {
 
         let license = video_widevine::License::decode(license_raw.as_slice())?;
 
-        self.load_keys(license)
-    }
-
-    fn load_keys(&mut self, license: video_widevine::License) -> Result<(), LicenseError> {
-        let keys = self.keys.unwrap_keys();
-
+        let mut added_keys = false;
         for key in license.key {
             let (Some(iv), Some(mut data)) = (key.iv, key.key) else {
                 continue;
             };
 
             let decryptor =
-                cbc::Decryptor::<aes::Aes128>::new_from_slices(&keys.encryption, &iv).unwrap();
+                cbc::Decryptor::<aes::Aes128>::new_from_slices(&session_keys.encryption, &iv)
+                    .unwrap();
             let new_size = decryptor
                 .decrypt_padded_mut::<aes::cipher::block_padding::Pkcs7>(data.as_mut_slice())?
                 .len();
@@ -226,13 +225,11 @@ impl Session {
             };
 
             info!("Loaded content key: {}", &new_key);
-            self.content_keys.push(new_key);
+            keys.push(new_key);
+            added_keys = true;
         }
-        Ok(())
-    }
 
-    pub fn content_keys(&self) -> &Vec<ContentKey> {
-        &self.content_keys
+        Ok(added_keys)
     }
 }
 

commit b97b9a7f759f775ebd896f5f0a67df89391dff82

Only send OnSessionKeysChange() if we have new keys
commit 3ba7dc019518 ("Store keys in OpenWv, simplify code in Session")
broke this. Although in practice I don't think it matters because we
only update each session once.

src/openwv.rs
@@ -308,14 +308,17 @@ impl cdm::ContentDecryptionModule_10_methods for OpenWv {
         };
 
         let response_raw = unsafe { slice_from_c(response, response_size as _) }.unwrap();
-        if let Err(e) = sess.load_license_keys(response_raw, &mut self.keys) {
-            self.throw(promise_id, &e);
-            return;
-        }
+        let new_keys = match sess.load_license_keys(response_raw, &mut self.keys) {
+            Err(e) => {
+                self.throw(promise_id, &e);
+                return;
+            }
+            Ok(b) => b,
+        };
 
         self.host.as_mut().OnResolvePromise(promise_id);
 
-        if !self.keys.is_empty() {
+        if new_keys {
             // Build an array of KeyInformation structs that point into keys.
             let key_infos: Vec<cdm::KeyInformation> = self
                 .keys
@@ -332,7 +335,7 @@ impl cdm::ContentDecryptionModule_10_methods for OpenWv {
                 self.host.as_mut().OnSessionKeysChange(
                     session_id,
                     session_id_size,
-                    true,
+                    new_keys,
                     key_infos.as_ptr(),
                     key_infos.len() as _,
                 );