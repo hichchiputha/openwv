7e611cf3749c49e08ab1ca9aa033461ce8ae695c
Thomas Hebb <tommyhebb@gmail.com>
2025-03-17 23:30:13 -0400

    build.rs: Don't construct PathBuf from components
    I'd done this for OS-independence, but we already hardcode the "/"
    separator elsewhere in this same file, and Windows handles it fine. So
    just do the more readable thing.

--- build.rs
+++ build.rs
@@ -12,10 +12,9 @@ enum BuildError {
 fn main() -> Result<(), BuildError> {
     let bindings_rs = "src/lib.rs";
 
-    let mut autocxx =
-        autocxx_build::Builder::new(bindings_rs, &[PathBuf::from_iter(["third-party", "cdm"])])
-            .extra_clang_args(&["-std=c++20"])
-            .build()?;
+    let mut autocxx = autocxx_build::Builder::new(bindings_rs, &[PathBuf::from("third-party/cdm")])
+        .extra_clang_args(&["-std=c++20"])
+        .build()?;
     autocxx.std("c++20").compile("cdm-api");
     println!("cargo:rerun-if-changed={}", bindings_rs);
 
