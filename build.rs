b66964fef96a2089a47ea6b6642dffa71ecd7c07
Thomas Hebb <tommyhebb@gmail.com>
2025-03-17 23:29:09 -0400

    build.rs: Set cc standard version with std() for Windows compatibility
    There's already a helper to abstract over the OS-specific flags, so use
    it instead of passing a raw flag.

--- build.rs
+++ build.rs
@@ -16,7 +16,7 @@ fn main() -> Result<(), BuildError> {
         autocxx_build::Builder::new(bindings_rs, &[PathBuf::from_iter(["third-party", "cdm"])])
             .extra_clang_args(&["-std=c++20"])
             .build()?;
-    autocxx.flag("-std=c++20").compile("cdm-api");
+    autocxx.std("c++20").compile("cdm-api");
     println!("cargo:rerun-if-changed={}", bindings_rs);
 
     let no_paths: [&str; 0] = [];
